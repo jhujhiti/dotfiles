c63f039fe79171667b7d6464138dddd6393baa37
Erick Turnquist <jhujhiti@adjectivism.org>
2009-07-25 17:01:39 -0400

    mkreverseldif.pl improvements

--- scripts/dns/mkreverseldif.pl
+++ scripts/dns/mkreverseldif.pl
@@ -2,8 +2,8 @@
 
 # Use this script to generate PowerDNS-compatible LDIF (dnsDomain2) for the
 # specified reverse domain. Common usage as such:
-# ./mkreverseldif.pl 0.10.in-addr.arpa ou=hosts,dc=adjectivism,dc=org | \
-#     ldapadd -c [otheropts]
+# ./mkreverseldif.pl 3.6.0.10.in-addr.arpa maya.adjectivism.org \
+#     ou=hosts,dc=adjectivism,dc=org | ldapadd -c [otheropts]
 # -c is necessary to ldapadd to make sure the operation doesn't fail if some
 # of the parent objects already exist (which is very likely)
 #
@@ -12,8 +12,9 @@
 
 use strict;
 
-die "no base specified" if !$ARGV[1];
-my $base = $ARGV[1];
+die "no base specified" if !$ARGV[2];
+my $base = $ARGV[2];
+my $ptr = $ARGV[1];
 my $arpa = $ARGV[0];
 
 my @parts = reverse split /\./, $arpa;
@@ -21,18 +22,45 @@ my @parts = reverse split /\./, $arpa;
 my $cumm_dn = "$base";
 my $cumm_domain = '';
 
-foreach(@parts) {
-    print <<END;
-dn: dc=$_,$cumm_dn
+sub parent {
+    my $x = shift;
+    return <<END;
+dn: dc=$x,$cumm_dn
 objectClass: dnsDomain
 objectClass: dnsDomain2
 objectClass: domain
 objectClass: domainRelatedObject
 objectClass: top
-associatedDomain: $_.$cumm_domain
-dc: $_
+associatedDomain: $x$cumm_domain
+dc: $x
 
 END
+}
+
+sub ptr {
+    my $x = shift;
+    return <<END;
+dn: dc=$x,$cumm_dn
+objectClass: dnsDomain
+objectClass: dnsDomain2
+objectClass: domain
+objectClass: domainRelatedObject
+objectClass: top
+associatedDomain: $x$cumm_domain
+pTRRecord: $ptr
+dc: $x
+
+END
+}
+
+my $i = -1;
+foreach(@parts) {
+    $i++;
+    if($i == $#parts) {
+        print ptr($_);
+    } else {
+        print parent($_);
+    }
     $cumm_dn = "dc=$_,$cumm_dn";
-    $cumm_domain = "$_.$cumm_domain";
+    $cumm_domain = ".$_$cumm_domain";
 }
