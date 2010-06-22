054711574ae4f5491e0365f71b6f8f5c9b661d27
Erick Turnquist <jhujhiti@adjectivism.org>
2010-06-22 00:23:10 -0400

    i suck at bash

--- modules.d/bash/aliases.sh
+++ modules.d/bash/aliases.sh
@@ -2,13 +2,13 @@
 ### depends: module platform
 ### depends: interactive
 
-if linux -o nexenta; then
+if [ linux -o nexenta ]; then
     alias ls="ls --color=yes -AFh"
     alias ll="ls --color=yes -AFlh"
 elif [ -x /sw/bin/ls ]; then
     alias ls="/sw/bin/ls --color=yes -AFh"
     alias ll="/sw/bin/ls --color=yes -AFlh"
-elif freebsd -o darwin; then
+elif [ freebsd -o darwin ]; then
     alias ls="ls -AFGh"
     alias ll="ls -AFGhl"
 else
