2d894ff2252e3ef25ab2ca064fb91edcf83d08fe
Erick Turnquist <jhujhiti@adjectivism.org>
2011-07-04 05:54:00 +0000

    bash: better gpg-agent handling

--- modules.d/bash/gpg-agent.sh
+++ modules.d/bash/gpg-agent.sh
@@ -4,6 +4,15 @@
 ### depends: quick_which gpg-agent
 
 # start if we're not running
-# TODO: --use-standard-socket doesn't work with remote home directories
 gpg-agent > /dev/null 2>&1 ||
-    gpg-agent --use-standard-socket --daemon > /dev/null 2>&1
+    gpg-agent --daemon \
+    --write-env-file "${HOME}/.gpg-agent-info" > /dev/null 2>&1
+
+# read the info file and export its variables
+if [ -f "${HOME}/.gpg-agent-info" ]; then
+    . "${HOME}/.gpg-agent-info"
+    export GPG_AGENT_INFO
+    # uncomment these if we ever replace ssh-agent
+    #export SSH_AUTH_SOCK
+    #export SSH_AGENT_PID
+fi
