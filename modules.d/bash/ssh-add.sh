94d14316b8b05de36f095f279823860ec5025e05
Erick Turnquist <jhujhiti@adjectivism.org>
2011-11-14 07:58:17 +0000

    quiet ssh-add down

--- modules.d/bash/ssh-add.sh
+++ modules.d/bash/ssh-add.sh
@@ -4,7 +4,7 @@
 ### depends: quick_which ssh-add
 ### depends: [ -r ~/.ssh/id_rsa ]
 
-ssh-add -L | grep `awk '{ print $2; };' ~/.ssh/id_rsa.pub` > /dev/null
+ssh-add -L 2>/dev/null | grep `awk '{ print $2; };' ~/.ssh/id_rsa.pub` > /dev/null
 if [ $? -eq 1 ]; then
-    ssh-add ~/.ssh/id_rsa
+    ssh-add ~/.ssh/id_rsa 2>/dev/null
 fi
