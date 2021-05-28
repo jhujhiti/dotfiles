4b1da900502996816e70d296921c3085ef9e8e2b
Erick Turnquist <jhujhiti@adjectivism.org>
2021-05-28 14:27:12 -0400

    switch to python-lsp-server (pylsp)

--- emacs/init.el
+++ emacs/init.el
@@ -217,10 +217,10 @@ Example: (apply-mode-hook 'flymake-mode \"emacs-lisp\" \"haskell\")"
 
 ;; python
 (setq
- ;; lsp-pyls-server-command (concat user-emacs-directory "lsp-virtualenv/bin/pyls")
- lsp-pyls-plugins-pydocstyle-enabled t
- lsp-pyls-plugins-flake8-enabled t
- lsp-pyls-plugins-pylint-enabled t)
+ ;; lsp-pylsp-server-command (concat user-emacs-directory "lsp-virtualenv/bin/pylsp")
+ lsp-pylsp-plugins-pydocstyle-enabled t
+ lsp-pylsp-plugins-flake8-enabled t
+ lsp-pylsp-plugins-pylint-enabled t)
 (add-hook 'python-mode-hook 'lsp)
 ;(setq elpy-rpc-python-command "python3")
 ;(require 'flymake-python-pyflakes)
